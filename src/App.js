c675e080f2a82ea200b264d04ffb17ce04cc0574
cklatt01 <chikashi.kurata.test@gmail.com>
2022-01-24 23:08:56 +0900

    change6

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <p>
-        uploaded by cklatt
+         uploaded by cklatt
         </p>
         
         <a
